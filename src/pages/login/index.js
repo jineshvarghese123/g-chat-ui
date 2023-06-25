d2a9dc53a5ea7cc81447f6d1efdf2299290d2c37
Jinesh Varghese <jineshvarghese123@gmail.com>
2023-06-25 22:53:08 +0530

    UI Regristration and Login and with custom css changes

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+
 
 function Login() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const loginUser = async(e)=>{
+    console.log(user)
+
+  }
+   
   return (
-    <div>Login</div>
-  )
+    <div className="h-screen bg-primary flex items-center justify-center">
+      <div className="bg-white shadow-md p-5 flex flex-col gap-5 w-96">
+        <h1 className="text-2xl f uppercase font-semibold text-primary text-center">
+          G-Chat Login
+        </h1>
+        <hr />
+        <input
+          type="email"
+          value={user.email}
+          onChange={(e) => {
+            setUser({ ...user, email: e.target.value });
+          }} placeholder="enter your email"
+        ></input>
+        <input
+          type="password"
+          value={user.password}
+          onChange={(e) => {
+            setUser({ ...user, password: e.target.value });
+          }}
+          placeholder="enter your password"
+          onClick={loginUser}
+        ></input>
+        <button className="contained-btn bg-primary">Login</button>
+        <Link to={'/register'} className='underline underline-offset-1'> dont have an account ? Register</Link>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 function Register() {
+  const [user, setUser] = useState({
+ name:"",
+ email:"",
+ password :""
+  })
+
+  const registerUser = (e)=>{
+    console.log(user)
+
+  }
   return (
-    <div>Register</div>
+    <div className='h-screen bg-primary flex items-center justify-center'>
+      <div  className='bg-white shadow-md p-5 flex flex-col gap-5 w-96' >
+        <h1 className='text-2xl f uppercase font-semibold text-primary text-center'>G-Chat Register</h1>
+        <hr/>
+        <input type='text' value={user.name} 
+        onChange={(e)=>{setUser({...user, name:e.target.value})}}
+        placeholder='enter your name'></input>
+         <input type='email' value={user.email} 
+        onChange={(e)=>{setUser({...user, email:e.target.value})}}
+        placeholder='enter your email'></input>
+        <input type='password' value={user.password} 
+        onChange={(e)=>{setUser({...user, password:e.target.value})}}
+        placeholder='enter your password'></input>
+        <button className='contained-btn bg-primary'  onClick={registerUser}>Register</button>
+        <Link to={'/login'} className='underline underline-offset-1'> Already Have an account ? Login</Link>
+      </div>
+      
+    </div>
   )
 }
 
